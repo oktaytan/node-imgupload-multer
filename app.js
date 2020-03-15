ffa8db5b80668217df9fe1481a8c2a76b5815f6d
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2020-03-15 23:08:59 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+
+// Set storage engine
+const storage = multer.diskStorage({
+	destination: './public/uploads/',
+	filename: function(req, file, cb) {
+		cb(
+			null,
+			file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+		);
+	}
+});
+
+// Init upload
+const upload = multer({
+	storage: storage,
+	limits: { fileSize: 1000000 },
+	fileFilter: function(req, file, cb) {
+		checkFileType(file, cb);
+	}
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb) {
+	// Allowed extensions
+	const fileTypes = /jpeg|jpg|png|gif/;
+	// Check ext
+	const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+	// Check mime
+	const mimetype = fileTypes.test(file.mimetype);
+
+	if (mimetype && extname) {
+		return cb(null, true);
+	} else {
+		cb('Error: Images Only!');
+	}
+}
+
+// Init app
+const app = express();
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Public folder
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.post('/upload', (req, res) => {
+	upload(req, res, (err) => {
+		if (err) {
+			res.render('index', {
+				msg: err
+			});
+		} else {
+			if (req.file == undefined) {
+				res.render('index', {
+					msg: 'No File Selected!'
+				});
+			} else {
+				res.render('index', {
+					msg: 'File Uploaded!',
+					file: `uploads/${req.file.filename}`
+				});
+			}
+		}
+	});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
